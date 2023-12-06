9e561889d04a1dfdcf960a3795982d3192d352f2
Horlew-myde <mezamune@gmail.com>
2023-12-06 01:18:16 +0100

    navbar and app.scss code

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
 import {About, Footer, Header, Skills, Testimonial, Work} from './container';
-
+import {Navbar} from './components';
+import './App.scss'
 const App = () => {
   return (
     <div className="app">
+        <Navbar />
         <Header />
         <About />
         <Work />
-        <Testimonial />
         <Skills />
+        <Testimonial />
         <Footer />
     </div>
   )
